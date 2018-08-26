c3d6235ba25b3dbeec92a234c515457d08f56ba9
TheZoq2 <frans.skarman@gmail.com>
2018-08-26 10:39:39 +0200

    Make webserver serve frontend

--- server/src/web.rs
+++ server/src/web.rs
@@ -3,6 +3,9 @@ use http::{header, StatusCode};
 use serde_json;
 use std::thread;
 
+use std::fs::File;
+use std::io::prelude::*;
+
 use types::ReadingCollection;
 
 use error::{Result, ErrorKind};
@@ -27,12 +30,24 @@ fn handle_data_request_query(
     }
 }
 
+fn handle_index_request() -> Result<String> {
+    let mut file = File::open("frontend/output/index.html")?;
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    Ok(contents)
+}
+
 pub fn run_server(listen_address: String, port: u16, readings: ReadingCollection) {
     let server = Server::new(move |request, mut response| {
         let request_path = request.uri().path();
         let request_path_parts = request_path.split('/').collect::<Vec<_>>();
 
         let handled = match request_path_parts[1] {
+            "" => {
+                handle_index_request()
+            }
             "data" => {
                 handle_data_request_query(&request_path_parts, &readings)
             }
