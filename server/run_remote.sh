1b4e3ec5c80c4bd5c2181278128adcbd0dc61d63
TheZoq2 <frans.skarman@protonmail.com>
2020-07-22 14:19:59 +0200

    Exit server on command exit

--- server/run_remote.sh
+++ server/run_remote.sh
@@ -3,7 +3,15 @@
 REMOTE="boats.local"
 REMOTE_DIR="/home/frans/Documents/weather"
 TARGET="target/armv7-unknown-linux-musleabihf/debug/server"
+FRONTEND_TARGET="frontend/output/index.html"
+FRONTEND_DEST="${REMOTE_DIR}/frontend/output/index.html"
 
-scp $TARGET $REMOTE:$REMOTE_DIR
-ssh $REMOTE 'RUST_BACKTRACE=1 cd /home/frans/Documents/weather && ./server'
+echo "Building frontend"
+make -C frontend
+
+echo "Copying server exe"
+rsync $TARGET $REMOTE:$REMOTE_DIR
+echo "Copying frontend"
+rsync $FRONTEND_TARGET $REMOTE:$FRONTEND_DEST
+ssh -t $REMOTE 'RUST_BACKTRACE=1 cd /home/frans/Documents/weather && ./server'
 
